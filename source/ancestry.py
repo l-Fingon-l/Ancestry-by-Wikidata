fe783385a0932af986da385f150d34a82774e1a3
Fingon <70348701+l-Fingon-l@users.noreply.github.com>
2021-08-30 19:27:17 +0300

    "examined" number correction

--- source/ancestry.py
+++ source/ancestry.py
@@ -7,8 +7,10 @@ tablichka = set()
 def search(qid, depth, sqid, year):
     if qid in tablichka:
         return False
-    else:
-        tablichka.add(qid)
+    tablichka.add(qid)
+
+    global amount
+    amount += 1
 
     depth += 1
     url = 'https://www.wikidata.org/wiki/Special:EntityData/' + qid + '.json'
@@ -32,9 +34,6 @@ def search(qid, depth, sqid, year):
         birth_year = int(str(birth_date).split('-', 1)[0][1:])
     if birth_year < year:  # not a descendant for sure
         return False
-
-    global amount
-    amount += 1
 
     if qid == sqid:
         if 'en' in text['entities'][sqid]['labels']:
@@ -95,35 +94,3 @@ def ancestry(qid, sqid):
         print('\n' + name + ' is not a descendant of ' + search_name)
     print('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     print(str(amount) + ' royals were examined.')
-
-
-"""import wikipedia
-
-# get the section of a page. In this case the Plot description of Metropolis
-section = wikipedia.WikipediaPage('Metropolis (1927 film)').section('Plot')
-# that will return fairly clean text, but the next line of code
-# will help clean that up.
-section = section.replace('\n', '').replace("\'", "")
-print(section) """
-"""
-import requests
-import mwparserfromhell
-
-url = 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles' \
-      '=Albert_Einstein&format=json'
-
-res = requests.get(url)
-text = list(res.json()["query"]["pages"].values())[0]["revisions"][0]["*"]
-wiki = mwparserfromhell.parse(text)
-
-birth_data = wiki.filter_templates(matches="Birth date")[1]
-birth_year = birth_data.get(1).value
-birth_month = birth_data.get(2).value
-birth_day = birth_data.get(3).value
-
-death_data = wiki.filter_templates(matches="Death date")[1]
-death_year = death_data.get(1).value
-death_month = death_data.get(2).value
-death_day = death_data.get(3).value
-
-print(birth_year)"""
