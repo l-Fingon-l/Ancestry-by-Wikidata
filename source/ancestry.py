9a0eeee649ee11abf8c1da3d3eeb2295e267e9d3
Fingon <70348701+l-Fingon-l@users.noreply.github.com>
2021-08-30 12:31:31 +0300

    Willian&James

---
+++ source/ancestry.py
@@ -0,0 +1,129 @@
+import requests
+
+amount = 0
+tablichka = set()
+
+
+def search(qid, depth, sqid, year):
+    if qid in tablichka:
+        return False
+    else:
+        tablichka.add(qid)
+
+    depth += 1
+    url = 'https://www.wikidata.org/wiki/Special:EntityData/' + qid + '.json'
+    res = requests.get(url)
+    text = res.json()
+
+    if 'en' in text['entities'][qid]['labels']:
+        name = text['entities'][qid]['labels']['en']['value']
+    else:
+        name = qid
+    space = '|   ' * depth
+    print(space + '=>' + name)
+
+    # date of birth check
+    if 'P569' not in text['entities'][qid]['claims']:
+        birth_year = 999999
+    elif 'datavalue' not in text['entities'][qid]['claims']['P569'][0]['mainsnak']:
+        birth_year = 999999
+    else:
+        birth_date = text['entities'][qid]['claims']['P569'][0]['mainsnak']['datavalue']['value']['time']
+        birth_year = int(str(birth_date).split('-', 1)[0][1:])
+    if birth_year < year:  # not a descendant for sure
+        return False
+
+    global amount
+    amount += 1
+
+    if qid == sqid:
+        if 'en' in text['entities'][sqid]['labels']:
+            search_name = text['entities'][sqid]['labels']['en']['value']
+        else:
+            search_name = sqid
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print('\n    ' + search_name)
+        return True
+
+    if 'P22' in text['entities'][qid]['claims']:  # father
+        father = text['entities'][qid]['claims']['P22'][0]['mainsnak']['datavalue']['value']['id']
+        if search(father, depth, sqid, year):
+            print('              ||          ')
+            print('              \\/         ')
+            print('    ' + name)
+            return True
+    if 'P25' in text['entities'][qid]['claims']:  # mother
+        mother = text['entities'][qid]['claims']['P25'][0]['mainsnak']['datavalue']['value']['id']
+        if search(mother, depth, sqid, year):
+            print('              ||          ')
+            print('              \\/         ')
+            print('    ' + name)
+            return True
+
+    return False
+
+
+def ancestry(qid, sqid):
+    url = 'https://www.wikidata.org/wiki/Special:EntityData/' + sqid + '.json'
+    res = requests.get(url)
+    text = res.json()
+
+    P569 = text['entities'][sqid]['claims']
+    if 'P569' not in P569:
+        year = 0
+    else:
+        date = P569['P569'][0]['mainsnak']['datavalue']['value']['time']
+        year = int(str(date).split('-', 1)[0][1:])
+
+    en = text['entities'][sqid]['labels']
+    if 'en' in en:
+        search_name = en['en']['value']
+    else:
+        search_name = sqid
+
+    url = 'https://www.wikidata.org/wiki/Special:EntityData/' + qid + '.json'
+    res = requests.get(url)
+    text = res.json()
+    if 'en' in text['entities'][qid]['labels']:
+        name = text['entities'][qid]['labels']['en']['value']
+    else:
+        name = qid
+
+    print('\n    ' + name + '      <=>      ' + search_name)
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+    if not search(qid, -1, sqid, year):
+        print('\n' + name + ' is not a descendant of ' + search_name)
+    print('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print(str(amount) + ' royals were examined.')
+
+
+"""import wikipedia
+
+# get the section of a page. In this case the Plot description of Metropolis
+section = wikipedia.WikipediaPage('Metropolis (1927 film)').section('Plot')
+# that will return fairly clean text, but the next line of code
+# will help clean that up.
+section = section.replace('\n', '').replace("\'", "")
+print(section) """
+"""
+import requests
+import mwparserfromhell
+
+url = 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles' \
+      '=Albert_Einstein&format=json'
+
+res = requests.get(url)
+text = list(res.json()["query"]["pages"].values())[0]["revisions"][0]["*"]
+wiki = mwparserfromhell.parse(text)
+
+birth_data = wiki.filter_templates(matches="Birth date")[1]
+birth_year = birth_data.get(1).value
+birth_month = birth_data.get(2).value
+birth_day = birth_data.get(3).value
+
+death_data = wiki.filter_templates(matches="Death date")[1]
+death_year = death_data.get(1).value
+death_month = death_data.get(2).value
+death_day = death_data.get(3).value
+
+print(birth_year)"""
---
+++ source/main.py
@@ -0,0 +1,25 @@
+from ancestry import ancestry
+
+James = 'Q126188'
+William = 'Q36812'
+Diana = 'Q9685'
+Henrietta = 'Q4491946'
+Waldegrave = 'Q6144986'
+Mary = 'Q75249569'
+Emma_Watson = 'Q39476'
+James_I_of_Scotland = 'Q235234'
+Catherine_Gordon_Byron = 'Q63645174'
+Catherine_Innes = 'Q75502963'
+John_Lytton = 'Q335447'
+Ada_Lovelace = 'Q7259'
+Rose_Leslie = 'Q461132'
+Charles_II_of_England = 'Q122553'
+Sir_Ian_McKellen = 'Q170510'
+JRR = 'Q892'
+Simon_Tolkien = 'Q82075'
+Queen_Victoria = 'Q9439'
+Princess_Charlotte_of_Cambridge = 'Q18002970'
+The_Queen = 'Q9682'
+Patrick_Lion = 'Q7147046'
+
+ancestry(Simon_Tolkien, JRR)
